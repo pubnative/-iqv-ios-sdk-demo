4369f3d61bd671b802e2aa3b1cf4eac85cf92db4
gzach <gaylord.zach@pubnative.net>
2020-03-26 21:53:15 +0100

    Initial Commit

---
+++ iqv demo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  iqv demo
+//
+//  Created by Gaylord Zach on 26.03.20.
+//  Copyright © 2020 PubNative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ iqv demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iqv demo
+//
+//  Created by Gaylord Zach on 26.03.20.
+//  Copyright © 2020 PubNative. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
